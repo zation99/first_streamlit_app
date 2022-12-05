a691756d9b77ba820b4f174af8aafe3b3c43f1ea
zation99 <zation99@gmail.com>
2022-12-04 18:37:55 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+streamlit.code(code, language='python')
